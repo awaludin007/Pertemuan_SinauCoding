9f4e7e1749b2f7b1275f3d6c612d29b86d84c2e5
awaludin007 <awaludiniqbal145@gmai.com>
2024-07-12 18:23:20 +0700

    tugas8

---
+++ PertemuanKe8/src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { reactive } from 'vue'
+import Table from '../components/Table.vue'
+import Header from '../json/Header.json'
+import User from '../json/User.json'
+
+const tableData = reactive({
+  header: Header,
+  data: User
+})
+
+const hanldeTampilData = (data) => {
+  alert(
+    `Nama ${data.nama}, Posisi ${data.position}, No Telp. ${data.no_telp} , Email ${data.email} `
+  )
+}
+</script>
+
+<template>
+  <div>
+    <h3>Table View</h3>
+    <Table :dataTable="tableData" @tampilData="hanldeTampilData" />
+  </div>
+</template>
